eee84bf2dbeda1224ade43f6cab1c669896ce5ac
kimbyeongsu <kbsu147@naver.com>
2023-08-10 22:26:17 +0900

    react first push

--- foodfairy/src/App.js
+++ foodfairy/src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [names, setNames] = useState([]);
+  const [input, setInput] = useState('');
+  
+  const inputChange = (e) => {
+    setInput(e.target.value);
+  }
+
+  const buttonClick = (e) => {
+    setNames((prevState) => {
+      console.log('등록하기 전의 names값', prevState);
+      return [input, ...prevState];
+    })
+  }
+
+  const onRemove = name => {
+    setNames(names.filter((element) => element !== name))
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <header className="App-header">
+        <input type="text" value={input} onChange={inputChange}/>
+        <button onClick={buttonClick}>추가</button>
+        <button>생성</button>
+        {names.map((name, index) => {
+          return (<div key={index}>
+            {name}
+            <button onClick={() => onRemove(name)}>X</button>
+            </div>)
+        })}
       </header>
     </div>
   );
